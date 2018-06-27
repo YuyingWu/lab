753799f1558ea5d47649e9418c0f6a2235b3a03b
jiwu.wyy <jiwu.wyy@alibaba-inc.com>
2018-06-27 13:10:55 +0800

    graphql: schema setup

--- graphql-playlist/server/graphql-server.js
+++ graphql-playlist/server/graphql-server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
-    // pass in a schema property
+  schema
 }));
 
 app.listen(5000, () => {
-    console.log('now listening for requests on port 5000, http://127.0.0.1:5000/');
+  console.log('now listening for requests on port 5000, http://127.0.0.1:5000/');
 });
